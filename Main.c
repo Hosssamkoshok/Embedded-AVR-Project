a25f789552675d42b9eab6545fc4a08659579888
Hosssamkoshok <150596255+Hosssamkoshok@users.noreply.github.com>
2024-08-17 17:49:55 +0300

    First step for a Man

---
+++ Main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   Main.c
+ * Author: Koshok
+ *
+ * Created on August 17, 2024, 4:43 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
---
+++ build/default/production/Main.o.d
@@ -0,0 +1,34 @@
+build/default/production/Main.o.d build/default/production/Main.o: Main.c \
+ d:\program\ files\avr\avr\include\avr\io.h \
+ d:\program\ files\avr\avr\include\avr\sfr_defs.h \
+ d:\program\ files\avr\avr\include\inttypes.h \
+ d:\program\ files\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\program\ files\avr\avr\include\stdint.h \
+ D:/Program\ Files/packs/Microchip/ATmega_DFP/3.1.264/xc8/avr/include/avr/iom32a.h \
+ d:\program\ files\avr\avr\include\avr\portpins.h \
+ d:\program\ files\avr\avr\include\avr\common.h \
+ d:\program\ files\avr\avr\include\avr\version.h \
+ d:\program\ files\avr\avr\include\avr\fuse.h \
+ d:\program\ files\avr\avr\include\avr\lock.h
+
+d:\program\ files\avr\avr\include\avr\io.h:
+
+d:\program\ files\avr\avr\include\avr\sfr_defs.h:
+
+d:\program\ files\avr\avr\include\inttypes.h:
+
+d:\program\ files\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\program\ files\avr\avr\include\stdint.h:
+
+D:/Program\ Files/packs/Microchip/ATmega_DFP/3.1.264/xc8/avr/include/avr/iom32a.h:
+
+d:\program\ files\avr\avr\include\avr\portpins.h:
+
+d:\program\ files\avr\avr\include\avr\common.h:
+
+d:\program\ files\avr\avr\include\avr\version.h:
+
+d:\program\ files\avr\avr\include\avr\fuse.h:
+
+d:\program\ files\avr\avr\include\avr\lock.h:
